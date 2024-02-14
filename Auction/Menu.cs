928a8af615af1bc4ef4c41eecc3831d1fef19a7f
Rolando Medina <rolando.ms@outlook.com>
2024-02-14 01:53:54 -0500

    feat: add helper commands for the CLI

--- Auction/Menu.cs
+++ Auction/Menu.cs
@@ -51,6 +51,12 @@ namespace Auction
                     case "peer -l":
                         PeersList();
                         break;
+                    case "clear":
+                        Console.Clear();
+                        break;
+                    case "help":
+                        Help();
+                        break;
                     default:
                         break;
                 }
@@ -181,6 +187,18 @@ namespace Auction
             {
                 Console.WriteLine($" -> {peer.Value} @ {peer.Key}");
             }
+        }
+
+        private static void Help()
+        {
+            Console.WriteLine("List of commands in this CLI: ");
+            Console.WriteLine("auction -i -> Initializes a new auction. An auction ID will be provided. Keep it close.");
+            Console.WriteLine("auction -b -> Places a new bid into an existing auction.");
+            Console.WriteLine("auction -c -> Completes an ongoing auction.");
+            Console.WriteLine("auction -l -> Lists all the existing auctions.");
+            Console.WriteLine("peer -l -> Lists all the existing known peers.");
+            Console.WriteLine("clear -> Clears the console");
+            Console.WriteLine("help -> Need more help? Feel free to reach out to @rmed1na :)");
         }
     }
 }
